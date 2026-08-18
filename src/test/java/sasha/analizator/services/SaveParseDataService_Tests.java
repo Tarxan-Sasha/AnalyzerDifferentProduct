95bbc68ecd9ddf343d996f354806c9c4aa152d6d
Tarxan <tarxan090807@gmail.com>
2026-08-18 20:42:13 +0300

    Реализованы полностью тесты для SaveParseDataService и ParseDataService

--- src/test/java/sasha/analizator/services/SaveParseDataService_Tests.java
+++ src/test/java/sasha/analizator/services/SaveParseDataService_Tests.java
@@ -2,6 +2,7 @@ package sasha.analizator.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -39,6 +40,50 @@ public class SaveParseDataService_Tests {
 
         verify(productRepository, times(1)).findByNameAndLink(newProduct.getName(), newProduct.getLink());
         verify(productRepository, times(1)).save(newProduct);
+
+    }
+    @Test
+    public void saveDataFromParse_completeUpdatePriseForExistProduct(){
+        Product newProduct = new Product();
+        newProduct.setName("TestName");
+        newProduct.setPrice("100грн");
+        newProduct.setLink("TestLink");
+
+        Product oldProduct = new Product();
+        oldProduct.setPrice("10грн");
+        Optional<Product> optionalOldProduct = Optional.of(oldProduct);
+
+
+        when(productRepository.findByNameAndLink(newProduct.getName(), newProduct.getLink())).thenReturn(Optional.of(oldProduct));;
+
+        saveParseData.saveDataFromParse(newProduct);
+
+        Assertions.assertTrue(optionalOldProduct.isPresent());
+        Assertions.assertEquals("100грн", optionalOldProduct.get().getPrice());
+
+        verify(productRepository, times(1)).findByNameAndLink(newProduct.getName(), newProduct.getLink());
+        verify(productRepository, never()).save(any(Product.class));
+    }
+
+    @Test
+    public void saveDataFromParse_completeWhenProductExistAndHaveLastPrice(){
+        Product newProduct = new Product();
+        newProduct.setName("TestName");
+        newProduct.setPrice("100грн");
+        newProduct.setLink("TestLink");
+
+        Product oldProduct = new Product();
+        oldProduct.setPrice("100грн");
+        Optional<Product> optionalOldProduct = Optional.of(oldProduct);
+
+        when(productRepository.findByNameAndLink(newProduct.getName(), newProduct.getLink())).thenReturn(Optional.of(oldProduct));;
+
+        saveParseData.saveDataFromParse(newProduct);
+
+        Assertions.assertEquals(oldProduct.getPrice(), optionalOldProduct.get().getPrice());
+
+        verify(productRepository, times(1)).findByNameAndLink(newProduct.getName(), newProduct.getLink());
+        verify(productRepository, never()).save(any(Product.class));
 
     }
 
